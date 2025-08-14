70884234522733de08463293b31d706d8d085c2f
Oluwatobiloba <tobentra32@gmail.com>
2025-08-14 12:41:13 -0500

    retry

--- app/page.js
+++ app/page.js
@@ -1,11 +1,16 @@
-import Image from "next/image";
-import Category from "./components/Category";
+import { getApartments } from './services/blockchain';
+import { Category, Collection } from './components/index';
 
-export default function Home() {
+export default async function Home() {
+
+  const apartmentsData = await getApartments();
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Category />
+      <Collection appartments={apartmentsData} />
       
     </div>
   );
 }
+// Note: The above code assumes that the `getApartments` function is correctly implemented to fetch apartment data from the blockchain.
