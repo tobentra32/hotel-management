ed444bcb07d4c575a52f176a765bf7e62d02fe06
Oluwatobiloba <tobentra32@gmail.com>
2025-08-14 11:19:06 -0500

    layout updt

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import ContextProvider from "./context";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
