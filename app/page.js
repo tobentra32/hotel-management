78aa30ec59951cf57a9491beb04725cd5b04fd82
Oluwatobiloba <tobentra32@gmail.com>
2025-08-14 13:48:11 -0500

    retry

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
+"use client"
 import { getApartments } from './services/blockchain';
-import Category from './components/Category';
+import { Category, Collection } from './components/index';
 
 export default async function Home() {
 
@@ -8,7 +9,7 @@ export default async function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Category />
-      
+      <Collection appartments={apartmentsData} />
       
     </div>
   );
